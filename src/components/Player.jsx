25a836029629e07faf4a8fa4a6d0086209303a5c
Alexander <dr.ambercat@gmail.com>
2022-12-13 18:39:27 +0300

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from 'react'
+import { useSphere } from '@react-three/cannon'
+import { useFrame, useThree } from '@react-three/fiber'
+import { Vector3 } from 'three'
+import { useKeyboard } from '../hooks/useKeyboard.jsx'
+
+const Player = () => {
+  const actions = useKeyboard()
+
+  const { camera } = useThree()
+  const [ref, api] = useSphere(() => ({
+    mass: 1,
+    type: 'Dynamic',
+    position: [0, 1, 0],
+  }))
+
+  const vel = useRef([0, 0, 10])
+
+  useEffect(() => {
+    api.velocity.subscribe((v) => (vel.current = v))
+  }, [api.velocity])
+
+  const pos = useRef([0, 0, 10])
+
+  useEffect(() => {
+    api.position.subscribe((p) => (pos.current = p))
+  }, [api.position])
+
+  useFrame(() => {
+    camera.position.copy(
+      new Vector3(pos.current[0], pos.current[1], pos.current[2])
+    )
+    // api.velocity.set(0, 1, 0) // constanlty lifting up
+  })
+
+  return <mesh ref={ref}></mesh>
+}
+
+export default Player
